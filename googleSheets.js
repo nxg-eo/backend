68b164d24bced3cdb57de370012535bb3215c677
5Devanshu <devanshudandekar5@gmail.com>
2026-01-13 23:24:14 +0530

    Revert "Working Live"
    This reverts commit 65b88220d3fefdbfffcf3813fc658c4d06f76947.

--- googleSheets.js
+++ googleSheets.js
@@ -61,13 +61,12 @@ async function writePaidRegistrationToSheet(registrationData) {
         registrationData.noShowConsent,
         registrationData.penaltyAmount,
         registrationData.registrationStatus,
-        registrationData.qrCodeUrl, // QR Code Link column
       ],
     ];
 
     await sheets.spreadsheets.values.append({
       spreadsheetId: PAID_REGISTRATION_SPREADSHEET_ID,
-      range: 'Sheet1!A:O', // 15 columns (added QR Code Link)
+      range: 'Sheet1!A:N', // 14 columns
       valueInputOption: 'USER_ENTERED',
       requestBody: { values },
     });
